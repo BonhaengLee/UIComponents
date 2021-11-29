9acdfca9766249f7ce161451e934eaeedc166841
ULemong <maintain9912@gmail.com>
2021-11-29 17:02:53 +0900

    Feat. add increase & decrease button

---
+++ counter/index.js
@@ -0,0 +1,12 @@
+const $increaseBtn = document.querySelector('.increase');
+const $decreaseBtn = document.querySelector('.decrease');
+const $counter = document.querySelector('.counter');
+
+const increase = () => $counter.textContent = 
+const decrease = () => $counter.textContent = 
+
+const counter = (())
+
+
+$increaseBtn.onclick = increase();
+$decreaseBtn.onclick = decrease();
