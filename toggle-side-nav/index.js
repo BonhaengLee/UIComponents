a7fea68e00d557d552304e4b4b051e7c9ba21cc2
BonhaengLee <bon952014@ajou.ac.kr>
2021-11-29 12:06:01 +0900

    feat. toggle side-nav

--- toggle-side-nav/index.js
+++ toggle-side-nav/index.js
@@ -1,1 +1,17 @@
-const q = document.querySelector("li");
+const $toggleButton = document.querySelector(".toggle.bx.bx-right-arrow-circle");
+
+const toggle = sessionStorage.getItem("toggle") === "true";
+
+const render = () => {
+  const $sideNav = document.getElementById("side-nav");
+  if (toggle) $sideNav.classList.remove("active");
+  else $sideNav.classList.add("active");
+};
+
+render();
+
+$toggleButton.onclick = () => {
+  console.log(toggle, sessionStorage.getItem("toggle"), !toggle);
+  sessionStorage.setItem("toggle", !toggle);
+  render();
+};
