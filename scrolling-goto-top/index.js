a466287438e8202bf83423baf70dee4788d765db
BonhaengLee <bon952014@ajou.ac.kr>
2021-11-29 16:23:14 +0900

    Feat. scrollToTop with throttle(100ms)

--- scrolling-goto-top/index.js
+++ scrolling-goto-top/index.js
@@ -1,8 +1,20 @@
 const $scrollIcon = document.querySelector(".scroll-icon");
 
-window.addEventListener("scroll", () => {
+const throttle = (callback, delay) => {
+  let timerId;
+
+  return () => {
+    if (timerId) return;
+    timerId = setTimeout(() => {
+      callback();
+      timerId = null;
+    }, delay);
+  };
+};
+
+window.onscroll = throttle(() => {
   if (window.pageYOffset < 100) $scrollIcon.style.display = "none";
   else $scrollIcon.style.display = "block";
-});
+}, 100);
 
-$scrollIcon.addEventListener("click", () => window.scrollTo(0, 0));
+$scrollIcon.onclick = () => window.scrollTo(0, 0);
