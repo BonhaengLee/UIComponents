197cb2016ce9fce9cc7c1efca9989752b1564952
BonhaengLee <bon952014@ajou.ac.kr>
2021-11-30 12:34:04 +0900

    Feat. start and stop(no reset and lap)

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -2,86 +2,56 @@ const $display = document.querySelector('.display');
 const $start = document.querySelector('button.control:nth-of-type(1)');
 const $reset = document.querySelector('button.control:nth-of-type(2)');
 
-// const p = (function () {
-//   // if ($start.textContent === 'Stop') clearInterval(timeoutId);
-//   let mm = 0;
-//   let ss = 0;
-//   let ms = 0;
-//   const printTime = () => {
-//     let mmVal = 0;
-//     let ssVal = 0;
-//     let msVal = 0;
+const p = (function () {
+  let mm = 0;
+  let ss = 0;
+  let ms = 0;
 
-//     if (mm < 10) {
-//       mmVal = '0' + mm;
-//     } else mmVal = mm;
-//     if (ss < 10) {
-//       ssVal = '0' + ss;
-//     } else ssVal = ss;
-//     if (ms < 10) {
-//       msVal = '0' + ms;
-//     } else msVal = ms;
+  const printTime = () => {
+    let mmVal = 0;
+    let ssVal = 0;
+    let msVal = 0;
 
-//     if (ms < 99) {
-//       ms++;
-//     } else {
-//       ms = 0;
-//       ss++;
-//       if (ss === 60) {
-//         ss = 0;
-//         mm++;
-//       }
-//     }
-//     $display.textContent = `${mmVal}:${ssVal}:${msVal}`;
-//   };
+    if (mm < 10) {
+      mmVal = '0' + mm;
+    } else mmVal = mm;
+    if (ss < 10) {
+      ssVal = '0' + ss;
+    } else ssVal = ss;
+    if (ms < 10) {
+      msVal = '0' + ms;
+    } else msVal = ms;
 
-//   return printTime;
-// })();
+    if (ms < 99) {
+      ms++;
+    } else {
+      ms = 0;
+      ss++;
+      if (ss === 60) {
+        ss = 0;
+        mm++;
+      }
+    }
+    $display.textContent = `${mmVal}:${ssVal}:${msVal}`;
+  };
+  return printTime;
+})();
+
+let id = null;
+
+function start() {
+  id = setInterval(p, 10);
+}
+
+function stop() {
+  clearTimeout(id);
+}
 
 let toggle = false;
 $start.onclick = () => {
   // console.log('on');
   toggle = !toggle;
   $start.textContent = toggle ? 'Stop' : 'Start';
-
-  let timeoutId;
-  // timeoutId = setInterval(p, 10, timeoutId);
-  const timeoutId = setInterval(() => {
-    const p = (function () {
-      // if ($start.textContent === 'Stop') clearInterval(timeoutId);
-      let mm = 0;
-      let ss = 0;
-      let ms = 0;
-      const printTime = () => {
-        let mmVal = 0;
-        let ssVal = 0;
-        let msVal = 0;
-
-        if (mm < 10) {
-          mmVal = '0' + mm;
-        } else mmVal = mm;
-        if (ss < 10) {
-          ssVal = '0' + ss;
-        } else ssVal = ss;
-        if (ms < 10) {
-          msVal = '0' + ms;
-        } else msVal = ms;
-
-        if (ms < 99) {
-          ms++;
-        } else {
-          ms = 0;
-          ss++;
-          if (ss === 60) {
-            ss = 0;
-            mm++;
-          }
-        }
-        $display.textContent = `${mmVal}:${ssVal}:${msVal}`;
-      };
-
-      return printTime;
-    })();
-  }, 10);
+  toggle ? start() : stop();
   // if ($start.textContent === 'Stop') clearInterval(timeoutId);
 };
