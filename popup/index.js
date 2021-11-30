a245696a16a14edaca73c2237b1d540dc3e1e63b
BonhaengLee <bon952014@ajou.ac.kr>
2021-11-30 17:03:29 +0900

    Feat. show popup-message(no layout, no remove prev message)

--- popup/index.js
+++ popup/index.js
@@ -1,6 +1,34 @@
-$toggleButton = document.querySelector('.toggle-button');
-$container = document.querySelector('.popup-container');
+const $toggleButton = document.querySelector('.toggle-button');
+const $popupContainer = document.querySelector('.popup-container');
+const $closeButton = document.querySelector('.close-button');
+const $cancelButton = document.querySelector('.cancel-button');
+const $okButton = document.querySelector('.ok-button');
+const $popupMessage = document.querySelector('.popup-message');
+const $popupInput = document.querySelector('.popup-input');
 
 $toggleButton.onclick = () => {
-  $container.style.display = 'block';
+  $popupContainer.style.display = $popupContainer.style.display === 'block' ? 'none' : 'block';
+};
+
+$closeButton.onclick = e => {
+  e.target.closest('div').style.display = 'none';
+};
+
+$cancelButton.onclick = e => {
+  e.target.closest('div').style.display = 'none';
+};
+
+$okButton.onclick = e => {
+  e.target.closest('div').style.display = 'none';
+  console.log(e.target.closest('input').value);
+
+  $popupMessage.textContent =
+    e.target.closest('input').value.trim() !== '' ? 'from popup : ' + e.target.closest('input').value : '';
+};
+
+$popupInput.onchange = e => {
+  console.log(e.target.value);
+  e.target.closest('div').style.display = 'none';
+  $popupMessage.textContent =
+    e.target.closest('input').value.trim() !== '' ? 'from popup : ' + e.target.closest('input').value : '';
 };
