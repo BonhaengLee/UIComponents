5f4aa3d6526f16dd0bdfbb926212bbea62256c09
BonhaengLee <bon952014@ajou.ac.kr>
2021-12-02 04:12:31 +0900

    Feat. form validation(no submitButtonToggle & toast)

--- form-validation/js/signin.js
+++ form-validation/js/signin.js
@@ -8,8 +8,6 @@ FormValidator.prototype.validateOnSubmit = function () {
     e.preventDefault();
     const self = this;
     this.fields.forEach(field => {
-      console.log(field);
-
       const input = document.querySelector(`#${field}`);
       self.validateFields(input);
     });
@@ -18,31 +16,56 @@ FormValidator.prototype.validateOnSubmit = function () {
 
 FormValidator.prototype.validateOnEntry = function () {
   this.fields.forEach(field => {
-    const input = document.querySelector(`#${field}`);
+    const $input = document.querySelector(`#${field}`);
     const self = this;
-    input.addEventListener('input', event => {
-      // console.log(event);
-
-      self.validateFields(input);
+    $input.addEventListener('input', event => {
+      self.validateFields($input);
     });
   });
 };
 
 FormValidator.prototype.validateFields = function (field) {
-  // console.log('vF', field.value, this.setStatus, field.value.trim() === '');
-  // console.log(field);
-
   if (field.value.trim() === '') {
     this.setStatus(field, `${field.name} is required.`, 'error');
   } else {
+    this.setStatus(field, null, 'success');
   }
+  // & : type = text (=== email)
+  if (field.type === 'text') {
+    const re = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    if (re.test(field.value)) {
+      this.setStatus(field, null, 'success');
+    } else {
+      this.setStatus(field, 'Email is not valid', 'error');
+    }
+  }
+  if (field.id === 'signin-password') {
+    const re = /^[0-9a-z]{6,12}$/;
+
+    if (re.test(field.value)) {
+      this.setStatus(field, null, 'success');
+    } else {
+      this.setStatus(field, 'Password is not valid', 'error');
+    }
+  }
+  // & : password-confirm (signup)
+  // if (field.id === 'signup-confirm-password') {
+  //   const passwordField = this.form.querySelector('#password');
+
+  //   if (field.value.trim() === '') {
+  //     this.setStatus(field, `password confirmation is required.`, 'error');
+  //   } else if (field.value !== passwordField.value) {
+  //     this.setStatus(field, `password confirmation is not matched.`, 'error');
+  //   } else {
+  //     this.setStatus(field, null, 'success');
+  //   }
+  // }
 };
 
 FormValidator.prototype.setStatus = function (field, message, type) {
   const successIcon = field.parentElement.querySelector('.icon-success.bx.bxs-check-circle');
   const errorIcon = field.parentElement.querySelector('.icon-error.bx.bxs-x-circle');
   const errorMessage = field.parentElement.querySelector('div.error');
-  console.log(field, successIcon, errorIcon, errorMessage);
 
   if (type === 'success') {
     if (errorIcon) {
@@ -51,7 +74,7 @@ FormValidator.prototype.setStatus = function (field, message, type) {
     if (errorMessage) {
       errorMessage.innerText = '';
     }
-    successIcon.classLiter.remove('hidden');
+    successIcon.classList.remove('hidden');
     field.parentElement.classList.remove('error'); //
   }
   if (type === 'error') {
