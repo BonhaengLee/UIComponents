81a19265b76c6db849eb6efefce6e2a66bbd08ed
BonhaengLee <bon952014@ajou.ac.kr>
2021-12-02 01:29:23 +0900

    Feat. infinite move left to right and right to left

--- carousel-slider/index.js
+++ carousel-slider/index.js
@@ -1,16 +1,9 @@
 const carousel = ($container, images) => {
   $container.innerHTML = `<div class="carousel-slides">
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-4.jpg" />
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-1.jpg" />
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-2.jpg" />
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-3.jpg" />
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-4.jpg" />
-        <img src="https://poiemaweb.com/assets/fs-images/exercise/movies/movie-1.jpg" />
+        ${images.map(image => `<img src="${image}" />`).join('')}      
       </div>
       <button class="carousel-control prev">&laquo;</button>
       <button class="carousel-control next">&raquo;</button>`;
-
-  // console.log($container);
 };
 
 carousel(document.querySelector('.carousel'), [
@@ -20,8 +13,27 @@ carousel(document.querySelector('.carousel'), [
   'https://poiemaweb.com/assets/fs-images/exercise/movies/movie-4.jpg"',
 ]);
 
+const $carouselSlides = document.querySelector('.carousel-slides');
+$carouselSlides.style.setProperty('--duration', '0.5s');
+
+const carouselImages = document.querySelectorAll('.carousel-slides img');
+const size = carouselImages[0].clientWidth;
+const $carousel = document.querySelector('.carousel');
+$carousel.style.opacity = 1;
+
 const $prevButton = document.querySelector('.carousel-control.prev');
 const $nextButton = document.querySelector('.carousel-control.next');
 
-$prevButton.onclick = () => {};
-$nextButton.onclick = () => {};
+let counter = 1;
+$nextButton.onclick = () => {
+  if (counter >= carouselImages.length - 1) return;
+  counter++;
+  $carouselSlides.style.setProperty('--currentSlide', `${size * counter}`);
+  console.log(counter, $carouselSlides.style.getPropertyValue('--currentSlide'));
+};
+
+$prevButton.onclick = () => {
+  if (counter <= 0) return;
+  counter--;
+  $carouselSlides.style.setProperty('--currentSlide', `${size * counter}`);
+};
