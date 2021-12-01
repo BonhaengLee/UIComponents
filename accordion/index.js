fcb1eb9a9f0daf7396e9f0dd8954834d7ef2e5f0
BonhaengLee <bon952014@ajou.ac.kr>
2021-12-01 11:15:22 +0900

    Feat. render()

--- accordion/index.js
+++ accordion/index.js
@@ -1,12 +1,31 @@
 const $accordion = document.querySelector('.accordion');
-const $menu = document.querySelectorAll('.menu');
+const $menuContainer = document.querySelector('.menu-container');
 
 $accordion.onclick = e => {
   [...$accordion.children].forEach(menu => {
     $accordion.classList.toggle('active', menu === e.target);
-    //   console.log(menu);
-    // });
   });
   console.log($accordion.children);
-  // console.log(e.target);
 };
+
+const render = () => {
+  [...$accordion.children].forEach(menuContainer => {
+    const $subMenu = menuContainer.children[1];
+    const $div = [...$subMenu.children];
+    console.log($div);
+
+    for (const x of $div) {
+      console.log(x.children[0].style.transition);
+      x.children[0].style.transition = 'none !important';
+    }
+    $subMenu.style.transition = menuContainer.classList.contains('active') && 'none !important';
+    $subMenu.style.height = menuContainer.classList.contains('active') && `${$subMenu.scrollHeight}px`;
+  });
+};
+
+render();
+
+/* 트랜지션 취소 */
+// .notransition {
+//   transition: none !important;
+// }
