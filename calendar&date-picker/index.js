8006dc70cb97eaf1ca90d0f299f9c080d2a5db24
ULemong <maintain9912@gmail.com>
2021-12-02 18:22:52 +0900

    Style. add choiceMonth's date & choiceToday

--- calendar&date-picker/index.js
+++ calendar&date-picker/index.js
@@ -21,8 +21,6 @@ const MONTH = [
   'December',
 ];
 
-// $calendarGrid.textContent =
-
 const today = new Date();
 
 $month.textContent = MONTH[today.getMonth()];
@@ -37,54 +35,68 @@ window.onclick = e => {
   $calendar.style.display = 'none';
 };
 
-let month = today.getMonth();
-let year = today.getFullYear();
+let thisMonth = today.getMonth();
+let thisYear = today.getFullYear();
 
-// & : 이달의 1일의 요일
-const firstDayOfThisMonth = new Date(year, month, 1).getDay();
-// & : 이달의 마지막 날짜
-const lastDateOfThisMonth = new Date(year, month + 1, 0).getDate();
-// & : 저번달의 마지막 날짜
-const lastDateOfLastMonth = new Date(year, month, 0).getDate();
 // & : days-grid
 const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-$calendarGrid.innerHTML = `<div class="days-grid">${dayOfWeek.map(v => `<div>${v}</div>`).join('')}</div>`;
-// & : date-grid
-const $dateGrid = document.createElement('div');
-$dateGrid.classList.add('date-grid');
-$calendarGrid.appendChild($dateGrid);
 
-for (let i = 1; i <= firstDayOfThisMonth; i++) {
-  $dateGrid.innerHTML += `<button><time datetime="2019-01-01">${
-    lastDateOfLastMonth - firstDayOfThisMonth + i
-  }</time></button>`;
-}
-for (let i = 1; i <= lastDateOfThisMonth; i++) {
-  $dateGrid.innerHTML += `<button><time datetime="2019-01-01">${i}</time></button>`;
-}
-// $calendarGrid.innerHTML += '</div>';
-console.log($calendarGrid.innerHTML);
+const showDate = (year, month) => {
+  $calendarGrid.innerHTML = `<div class="days-grid">${dayOfWeek.map(v => `<div>${v}</div>`).join('')}</div>`;
+  // & : date-grid
+  const $dateGrid = document.createElement('div');
+  $dateGrid.classList.add('date-grid');
+  $calendarGrid.appendChild($dateGrid);
+  // & : 이달의 1일의 요일
+  const firstDayOfThisMonth = new Date(year, month, 1).getDay();
+  // & : 이달의 마지막의 요일
+  const lastDayOfThisMonth = new Date(year, month + 1, 0).getDay();
+  // & : 이달의 마지막 날짜
+  const lastDateOfThisMonth = new Date(year, month + 1, 0).getDate();
+  // & : 저번달의 마지막 날짜
+  const lastDateOfLastMonth = new Date(year, month, 0).getDate();
+
+  for (let i = 1; i <= firstDayOfThisMonth; i++) {
+    $dateGrid.innerHTML += `<button class="last-next"><time datetime="2019-01-01">${
+      lastDateOfLastMonth - firstDayOfThisMonth + i
+    }</time></button>`;
+  }
+  for (let i = 1; i <= lastDateOfThisMonth; i++) {
+    if (i === today.getDate()) {
+      $dateGrid.innerHTML += `<button class="now"><time datetime="2019-01-01">${i}</time></button>`;
+    } else $dateGrid.innerHTML += `<button><time datetime="2019-01-01">${i}</time></button>`;
+  }
+  for (let i = 1; i <= 6 - lastDayOfThisMonth; i++) {
+    $dateGrid.innerHTML += `<button class="last-next"><time datetime="2019-01-01">${i}</time></button>`;
+  }
+};
 
 document.body.onload = () => {
+  showDate(thisYear, thisMonth);
+
   $next.onclick = () => {
-    if (month === 11) {
-      month = 0;
-      year++;
+    if (thisMonth === 11) {
+      thisMonth = 0;
+      thisYear++;
     } else {
-      month++;
+      thisMonth++;
     }
-    $month.textContent = MONTH[month];
-    $year.textContent = year;
+    showDate(thisYear, thisMonth);
+    $month.textContent = MONTH[thisMonth];
+    $year.textContent = thisYear;
   };
 
   $prev.onclick = () => {
-    if (month === 0) {
-      month = 11;
-      year--;
+    if (thisMonth === 0) {
+      thisMonth = 11;
+      thisYear--;
     } else {
-      month--;
+      thisMonth--;
     }
-    $month.textContent = MONTH[month];
-    $year.textContent = year;
+    showDate(thisYear, thisMonth);
+    $month.textContent = MONTH[thisMonth];
+    $year.textContent = thisYear;
   };
+
+  // $dateGrid.onclick = () => con
 };
